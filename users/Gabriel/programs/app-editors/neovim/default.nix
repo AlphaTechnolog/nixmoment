2afbb9c40053bc4e340613760ca7ac17fa21c7a5
Gabriel Guerra <gfranklings@gmail.com>
2024-05-21 19:18:54 -0400

    chore: nvim: adding autocomplete

--- users/Gabriel/programs/app-editors/neovim/default.nix
+++ users/Gabriel/programs/app-editors/neovim/default.nix
@@ -78,6 +78,31 @@
           rust-analyzer.enable = true;
         };
       };
+
+      cmp = {
+        enable = true;
+        autoEnableSources = true;
+
+        settings = {
+          sources = let
+            mk = name: { inherit name; };
+          in [
+            (mk "nvim_lsp")
+            (mk "path")
+            (mk "buffer")
+          ];
+
+          mapping = {
+            "<C-Space>" = "cmp.mapping.complete()";
+            "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+            "<C-e>" = "cmp.mapping.close()";
+            "<C-f>" = "cmp.mapping.scroll_docs(4)";
+            "<CR>" = "cmp.mapping.confirm({ select = true })";
+            "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+            "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+          };
+        };
+      };
     };
   };
 }

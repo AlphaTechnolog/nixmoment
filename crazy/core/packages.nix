544046225d6cf0d0d416440b1241c3f2cb731979
Gabriel Guerra <gfranklings@gmail.com>
2024-05-25 16:04:18 -0400

    chore: installing java

--- crazy/core/packages.nix
+++ crazy/core/packages.nix
@@ -1,13 +1,17 @@
-{ pkgs, ... }:
-
-{
+{ pkgs, ... }: {
   environment.systemPackages = with pkgs; [
     wget
     git
     dotnet-sdk
   ];
 
+  programs.java = {
+    enable = true;
+    package = pkgs.jdk22;
+  };
+
   environment.sessionVariables = {
     DOTNET_ROOT = "${pkgs.dotnet-sdk}";
+    _JAVA_OPTIONS = "-Dawt.useSystemAAFontSettings=lcd";
   };
 }

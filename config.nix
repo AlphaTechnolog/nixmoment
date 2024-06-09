320050c0022e48b803c90675fe03c5b29cd7ade9
Gabriel Guerra <gfranklings@gmail.com>
2024-06-09 15:04:01 -0400

    chore: switching to carbonfox

--- config.nix
+++ config.nix
@@ -31,7 +31,7 @@ rec {
   };
 
   # select a colorscheme definition from `./colorschemes/*.nix`.
-  metacolorscheme = import ./colorschemes/dawnfox.nix;
+  metacolorscheme = import ./colorschemes/carbonfox.nix;
 
   # exports the palette attribute of metacolorscheme to be able to call
   # the colors from the themeable applications, even the awm one.
